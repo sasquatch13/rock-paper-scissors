6534c34478e98d7beac5d91c2b40a87ded814a37
sasquatch13 <115385918+sasquatch13@users.noreply.github.com>
2023-01-16 00:20:16 -0500

    added responces needs debugging still

--- script.js
+++ script.js
@@ -14,3 +14,24 @@ function getComputerChoice() {
 }
 
 console.log(getComputerChoice());
+
+function round(playerSelection, computerSelection) {
+    computerSelection = getComputerChoice();
+    if (playerSelection == "paper" && computerSelection == "rock") {
+        return "You Lose! Paper beats Rock"
+    } else if (playerSelection == "rock" && computerSelection == "scissors") {
+        return "You Lose! Rock beats Scissors"
+    } else if (playerSelection == "scissors" && computerSelection == "paper") {
+        return "You Lose! Scissors beats Paper"
+    } else if (playerSelection == "paper" && computerSelection == "rock") {
+        return "You Win! Paper beats Rock"
+    } else if (playerSelection == "rock" && computerSelection == "scissors") {
+        return "You Win! Rock beats Scissors"
+    } else if (playerSelection == "scissors" && computerSelection == "paper")
+        {return "You Win! Scissors beats Paper" 
+    } else {return "You Tied!"}
+}
+
+console.log(round("paper"));
+console.log(round("rock"));
+console.log(round("scissors"));
