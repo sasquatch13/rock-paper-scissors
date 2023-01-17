8f997904e481ca835b50cec38c8a04d9ab58f182
sasquatch13 <115385918+sasquatch13@users.noreply.github.com>
2023-01-16 20:23:45 -0500

    answer not dependant on case

--- script.js
+++ script.js
@@ -17,21 +17,27 @@ console.log(getComputerChoice());
 
 function round(playerSelection, computerSelection) {
     computerSelection = getComputerChoice();
-    if (playerSelection == "rock" && computerSelection == "paper") {
+    if (playerSelection.toLowerCase() == "rock" && computerSelection == "paper") {
         return "You Lose! Paper beats Rock"
-    } else if (playerSelection == "scissors" && computerSelection == "rock") {
+
+    } else if (playerSelection.toLowerCase() == "scissors" && computerSelection == "rock") {
         return "You Lose! Rock beats Scissors"
-    } else if (playerSelection == "paper" && computerSelection == "scissors") {
+
+    } else if (playerSelection.toLowerCase() == "paper" && computerSelection == "scissors") {
         return "You Lose! Scissors beats Paper"
-    } else if (playerSelection == "paper" && computerSelection == "rock") {
+
+    } else if (playerSelection.toLowerCase() == "paper" && computerSelection == "rock") {
         return "You Win! Paper beats Rock"
-    } else if (playerSelection == "rock" && computerSelection == "scissors") {
+
+    } else if (playerSelection.toLowerCase() == "rock" && computerSelection == "scissors") {
         return "You Win! Rock beats Scissors"
-    } else if (playerSelection == "scissors" && computerSelection == "paper")
+
+    } else if (playerSelection.toLowerCase() == "scissors" && computerSelection == "paper")
         {return "You Win! Scissors beats Paper" 
+
     } else {return "You Tied!"}
 }
 
-console.log(round("paper"));
-console.log(round("rock"));
-console.log(round("scissors"));
+console.log(round("pAper"));
+console.log(round("rOck"));
+console.log(round("scIssors"));
