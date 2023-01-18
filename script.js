43ad43742f5171ab3a4d5e6319166fb12bafce5c
sasquatch13 <115385918+sasquatch13@users.noreply.github.com>
2023-01-18 00:44:14 -0500

    fixed random number genorator, started rounds code

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let decide = Math.random() *100;
+let decide = Math.floor(Math.random() * 100);
 console.log(decide);
 
 function getComputerChoice() {
@@ -14,37 +14,58 @@ function getComputerChoice() {
 }
 
 console.log(getComputerChoice());
-//let playerSelection = prompt("rock, paper, scissors");
-//prompt(playRound());
+var youWin = 0;
+var computerWin = 0;
 
 function playRound(playerSelection, computerSelection) {
     computerSelection = getComputerChoice();
     playerSelection = prompt("rock, paper, scissors");
     if (playerSelection.toLowerCase() == "rock" && computerSelection == "paper") {
+        computerWin++
         return "You Lose! Paper beats Rock"
 
     } else if (playerSelection.toLowerCase() == "scissors" && computerSelection == "rock") {
+        computerWin++
         return "You Lose! Rock beats Scissors"
 
     } else if (playerSelection.toLowerCase() == "paper" && computerSelection == "scissors") {
+        computerWin++
         return "You Lose! Scissors beats Paper"
 
     } else if (playerSelection.toLowerCase() == "paper" && computerSelection == "rock") {
+        youWin++
         return "You Win! Paper beats Rock"
+        
 
     } else if (playerSelection.toLowerCase() == "rock" && computerSelection == "scissors") {
+        youWin++
         return "You Win! Rock beats Scissors"
 
-    } else if (playerSelection.toLowerCase() == "scissors" && computerSelection == "paper")
-        {return "You Win! Scissors beats Paper" 
+    } else if (playerSelection.toLowerCase() == "scissors" && computerSelection == "paper") {
+        youWin++
+        return "You Win! Scissors beats Paper" 
 
     } else {return "You Tied!"}
 }
+console.log(playRound())
+//console.log(game());
 
-console.log(playRound());
+console.log(youWin);
+console.log(computerWin);
 
-function game() {
-    for (let i = 0; i < 5; i++) {
+//function game() {
+    //for (let i = 0; i < 5; i++) {
+        //playRound()
+        //console.log(getComputerChoice())
+            //if (youWin == 3) {
+            //    return "you win"
+            
+            //} else if (computerWin++ == 3) {
+            //    return "computer wins"
+            //}
         
-     }
-}
+    // }
+//}
+
+console.log(youWin);
+console.log(computerWin);
